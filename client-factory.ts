4286e85e7b22a0e9bfd79d3cacab3a29c997a1dd
Sergey Tihovskiy <artur.momo@gmail.com>
2019-01-30 13:59:18 +0200

    remove mocha

--- client-factory.ts
+++
@@ -1,5 +0,0 @@
-import { ThriftMaker } from './thrift-compiler';
-import { createXHRClient, createXHRConnection } from 'thrift';
-
-
-export const ClientFactory = ThriftMaker.getFactory(createXHRConnection, createXHRClient);
--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function kappa() {
-    console.log('hello');
-}
-exports.kappa = kappa;
